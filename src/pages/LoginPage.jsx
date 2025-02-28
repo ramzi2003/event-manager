f6f319ed6d60aa9566127a2718bd02eedade54df
Ramzi Shodmonbekov <ramzi.shodmonbekov_2026@ucentralasia.org>
2025-02-28 11:15:31 +0600

    Setting layouts' UI

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -103,7 +103,7 @@ function LoginPage() {
                 </div>
                 <button
                   type="submit"
-                  className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                  className="w-full cursor-pointer text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                 >
                   Sign in
                 </button>
